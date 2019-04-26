2681f2c2ad792572880f4dabf726be58accca12b
encloinc <jelch2002@gmail.com>
2019-04-26 00:13:50 -0500

    aa

--- opt/index.js
+++ opt/index.js
@@ -5,7 +5,8 @@ var http = require('http')
 
 // Get port of ngrok from ngrok.log
 var log = fs.readFileSync('serveo.log', {encoding: 'utf-8'})
-var port = log
+var port = log.split('\n')[2].split(' ')[2]
+
 const listener = http.createServer(function (request, response) {
   response.writeHead(200, { 'Content-Type': 'application/json' })
   response.end('Server Address: serveo.net:' + port)
