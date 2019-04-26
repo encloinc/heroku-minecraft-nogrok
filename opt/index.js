6e202f40b125456626958e0a456f601636115bdf
encloinc <jelch2002@gmail.com>
2019-04-26 08:37:13 -0500

    Added port choosing

--- opt/index.js
+++ opt/index.js
@@ -3,18 +3,24 @@
 var fs = require('fs')
 var http = require('http')
 log = null
-port = null
+ip = null
 // Get port of ngrok from ngrok.log
 function check(){
 	var log = fs.readFileSync('serveo.log', {encoding: 'utf-8'})
-	return log.split('\n')[2] !== undefined;
+	return log.split('\n')[1] !== undefined;
 }
 
 const inter = setInterval(function(){
 	console.log("Checking for serveo.log")
 	if(check()){
 		log = fs.readFileSync('serveo.log', {encoding: 'utf-8'})
-		port = log.split('\n')[2].split(' ')[2]
+		if(!log.includes('failed')){
+			
+			ip = log.split('\n')[3].split(' ')[4]
+		}else{
+
+			ip = 'serveo failed, port specified is probably used'
+		}
 		start()
 		clearInterval(inter)
 	}
@@ -24,7 +30,7 @@ const inter = setInterval(function(){
 function start(){
 	const listener = http.createServer(function (request, response) {
 	  response.writeHead(200, { 'Content-Type': 'application/json' })
-	  response.end('Server Address: serveo.net:' + port)
+	  response.end('Server Address: ' + ip)
 	}).listen(process.env.PORT || 8080, () => {
 	  console.log(`Listening on port: ${listener.address().port}`)
 	})
