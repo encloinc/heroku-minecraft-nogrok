12d517d0a79480c449b48bf179ac3885da1899bf
encloinc <jelch2002@gmail.com>
2019-04-25 22:51:30 -0500

    oh yeah boi

---
+++ opt/index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+var http = require('http')
+
+// Get port of ngrok from ngrok.log
+var log = fs.readFileSync('serveo.log', {encoding: 'utf-8'})
+var port = log.split('\n')[2].split(' ')[2]
+
+const listener = http.createServer(function (request, response) {
+  response.writeHead(200, { 'Content-Type': 'application/json' })
+  response.end('Server Address: serveo.net:' + port)
+}).listen(process.env.PORT || 8080, () => {
+  console.log(`Listening on port: ${listener.address().port}`)
+})
